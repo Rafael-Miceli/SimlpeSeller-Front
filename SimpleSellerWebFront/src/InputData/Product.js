39ecb5d5828997ad8b59605e5ff513b13ebac021
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-02-14 17:17:39 -0200

    Resolvido problema de required no load vir invalido

--- SimpleSellerWebFront/src/InputData/Product.js
+++ SimpleSellerWebFront/src/InputData/Product.js
@@ -31,13 +31,13 @@ class Product extends Component {
       <div>
         Criar Produto
         <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <input className="mdl-textfield__input" type="text" name="productName" id="productName" required />          
-          <label className="mdl-textfield__label" htmlFor="productName">Nome</label>          
+          <input className="mdl-textfield__input" type="text" name="productName" id="productName" onChange={(e) => {e.target.required = true}} />          
+          <label className="mdl-textfield__label" htmlFor="productName">Nome*</label>          
         </div>
         <div>
         <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
-          <input className="mdl-textfield__input" type="text" id="fornecedor" value="" readOnly tabIndex="-1" />
-          <label htmlFor="fornecedor" className="mdl-textfield__label">Fornecedor</label>
+          <input className="mdl-textfield__input" type="text" id="fornecedor" value="" readOnly tabIndex="-1" onChange={(e) => {e.target.required = true}} />
+          <label htmlFor="fornecedor" className="mdl-textfield__label">Fornecedor*</label>
           <ul htmlFor="fornecedor" className="mdl-menu mdl-menu--bottom-left mdl-js-menu">
               <li onClick={this.modalAdicionarFornecedor.bind(this)} className="mdl-menu__item">Criar novo <i className="material-icons">add</i></li>
               <li className="mdl-menu__item">Forn 1</li>
@@ -46,17 +46,19 @@ class Product extends Component {
         </div>                       
         </div>
         <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <input className="mdl-textfield__input" type="text" name="sellingPrice" id="sellingPrice" required pattern="-?[0-9]*(\.[0-9]+)?" />          
-          <label className="mdl-textfield__label" htmlFor="sellingPrice">Preço de Venda</label>       
+          <input className="mdl-textfield__input" type="text" name="sellingPrice" id="sellingPrice" pattern="-?[0-9]*(\.[0-9]+)?" onChange={(e) => {e.target.required = true}} />          
+          <label className="mdl-textfield__label" htmlFor="sellingPrice">Preço de Venda*</label>       
           <span className="mdl-textfield__error">Preço de venda deve ser um número!</span>   
         </div>
         <div></div>
         <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <input className="mdl-textfield__input" type="text" name="buyingPrice" id="buyingPrice" required pattern="-?[0-9]*(\.[0-9]+)?" />          
-          <label className="mdl-textfield__label" htmlFor="buyingPrice">Preço de Compra</label>       
+          <input className="mdl-textfield__input" type="text" name="buyingPrice" id="buyingPrice" pattern="-?[0-9]*(\.[0-9]+)?" onChange={(e) => {e.target.required = true}}/>          
+          <label className="mdl-textfield__label" htmlFor="buyingPrice">Preço de Compra*</label>       
           <span className="mdl-textfield__error">Preço de compra deve ser um número!</span>   
         </div>
-        <div>
+        <br />
+        <br />        
+        <div style={{textAlign: "center"}}>
           <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
             Criar
           </button>
